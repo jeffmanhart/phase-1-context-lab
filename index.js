e4a83cf9b790b91053def72658c7d7e76c20355a
jeffmanhart <jeffreymanhart@gmail.com>
2023-10-03 10:36:34 -0600

    modified the solution to have better context driven approach

--- index.js
+++ index.js
@@ -9,7 +9,70 @@
  for you to use if you need it!
  */
 
+
+
+function createEmployeeRecord(employee){
+    let empObj={
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return empObj;
+}
+
+function createEmployeeRecords(employeeArray){
+    let allEmployees = []
+    employeeArray.forEach(employee => {
+        allEmployees.push(new createEmployeeRecord(employee))
+    });
+
+    return allEmployees;
+}
+
+function createTimeInEvent(time){
+    let dateTime = time.split(' ')
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        date: dateTime[0],
+        hour: Number(dateTime[1])
+    })
+    return this
+}
+function createTimeOutEvent(time){
+    let dateTime = time.split(' ')
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        date: dateTime[0],
+        hour: Number(dateTime[1])
+    })
+    return this
+}
+
+const hoursWorkedOnDate=function (date){
+    const filterTimeIn = this.timeInEvents.find(e=>e.date ===date)
+    const filterTimeOut = this.timeOutEvents.find(e=>e.date ===date)
+
+    return (filterTimeOut.hour - filterTimeIn.hour ) / 100
+
+}
+function wagesEarnedOnDate(date){
+    const payRate = this.payPerHour
+    debugger
+    const hours = hoursWorkedOnDate.call(this, date)
+    return hours * payRate
+}
+
+function findEmployeeByFirstName(array,firstName){
+    return array.find(function(e){
+        return e.firstName === firstName
+    })
+}
+
 const allWagesFor = function () {
+    debugger
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -21,3 +84,10 @@ const allWagesFor = function () {
     return payable
 }
 
+let calculatePayroll = function(employees){
+    let payroll = 0
+    employees.forEach(empl=> {
+        payroll+= allWagesFor.call(empl)
+    });
+    return payroll
+}
